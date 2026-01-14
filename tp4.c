e3949a5938466d2aa886c87007a0974e882ecf19
MOHAMED LAMINE BELAROUCI <154247237+maxie-dz@users.noreply.github.com>
2026-01-14 13:27:20 +0100

    Update tp4.c

--- tp4.c
+++ tp4.c
@@ -1,90 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 typedef struct SNode {
     char data;
-    struct SNode *next;
+    struct SNode* next;
 } SNode;
 
 typedef struct QNode {
     char data;
-    struct QNode *next;
+    struct QNode* next;
 } QNode;
 
 typedef struct {
-    QNode *front;
-    QNode *rear;
+    QNode* front;
+    QNode* rear;
 } Queue;
 
-SNode* push(SNode *top, char c) {
-    SNode *n = malloc(sizeof(SNode));
-    n->data = c;
-    n->next = top;
-    return n;
+SNode* push(SNode* top, char ch) {
+    SNode* newNode = (SNode*)malloc(sizeof(SNode));
+    newNode->data = ch;
+    newNode->next = top;
+    return newNode;
 }
 
-SNode* pop(SNode *top, char *c) {
-    SNode *t = top;
-    *c = t->data;
-    top = t->next;
-    free(t);
+SNode* pop(SNode* top, char* ch) {
+    if (top == NULL) return NULL;
+    SNode* temp = top;
+    *ch = top->data;
+    top = top->next;
+    free(temp);
     return top;
 }
 
-void initQueue(Queue *q) {
-    q->front = q->rear = NULL;
-}
-
-void enqueue(Queue *q, char c) {
-    QNode *n = malloc(sizeof(QNode));
-    n->data = c;
-    n->next = NULL;
-
-    if (!q->rear)
-        q->front = q->rear = n;
-    else {
-        q->rear->next = n;
-        q->rear = n;
+void enqueue(Queue* q, char ch) {
+    QNode* newNode = (QNode*)malloc(sizeof(QNode));
+    newNode->data = ch;
+    newNode->next = NULL;
+    if (q->rear == NULL) {
+        q->front = q->rear = newNode;
+        return;
     }
+    q->rear->next = newNode;
+    q->rear = newNode;
 }
 
-int dequeue(Queue *q, char *c) {
-    if (!q->front) return 0;
-
-    QNode *t = q->front;
-    *c = t->data;
-    q->front = t->next;
-    if (!q->front) q->rear = NULL;
-    free(t);
+int dequeue(Queue* q, char* ch) {
+    if (q->front == NULL) return 0;
+    QNode* temp = q->front;
+    *ch = q->front->data;
+    q->front = q->front->next;
+    if (q->front == NULL) q->rear = NULL;
+    free(temp);
     return 1;
 }
 
 int main() {
-    char str[200], a, b;
-    SNode *top = NULL;
-    Queue q;
-
-    initQueue(&q);
+    char str[100];
+    printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;
 
-    for (int i = 0; str[i]; i++) {
-        if (isalpha(str[i])) {
-            char c = toupper(str[i]);
-            top = push(top, c);
-            enqueue(&q, c);
+    SNode* stack = NULL;
+    Queue queue = {NULL, NULL};
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = toupper(str[i]);
+        if (isalpha(c)) {
+            stack = push(stack, c);
+            enqueue(&queue, c);
         }
     }
 
-    while (top && q.front) {
-        top = pop(top, &a);
-        dequeue(&q, &b);
-        if (a != b) {
-            printf("Not Palindrome\n");
-            return 0;
+    int isPalindrome = 1;
+    char c1, c2;
+
+    while (stack != NULL && queue.front != NULL) {
+        stack = pop(stack, &c1);
+        dequeue(&queue, &c2);
+        if (c1 != c2) {
+            isPalindrome = 0;
+            break;
         }
     }
 
-    printf("Palindrome\n");
+    if (isPalindrome) printf("Palindrome\n");
+    else printf("Not Palindrome\n");
+
+    while (stack != NULL) {
+        stack = pop(stack, &c1);
+    }
+    while (queue.front != NULL) {
+        dequeue(&queue, &c1);
+    }
+
     return 0;
 }
